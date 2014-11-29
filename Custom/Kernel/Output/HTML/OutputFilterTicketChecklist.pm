a7b2d70d76bf8a818aa1096a7cfbab3e701a2d3f
reneeb <info@perl-services.de>
2014-11-29 11:41:37 +0100

    update output filter

--- Custom/Kernel/Output/HTML/OutputFilterTicketChecklist.pm
+++ Custom/Kernel/Output/HTML/OutputFilterTicketChecklist.pm
@@ -14,12 +14,14 @@ use warnings;
 
 use List::Util qw(first);
 
-use Kernel::System::Encode;
-use Kernel::System::Time;
-use Kernel::System::PerlServices::TicketChecklist;
-use Kernel::System::PerlServices::TicketChecklistStatus;
+our @ObjectDependencies = qw(
+    Kernel::System::Ticket
+    Kernel::System::Web::Request
+    Kernel::System::PerlServices::TicketChecklist
+    Kernel::System::PerlServices::TicketChecklistStatus
+);
 
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -28,27 +30,7 @@ sub new {
     my $Self = {};
     bless( $Self, $Type );
 
-    # get needed objects
-    for my $Object (
-        qw(MainObject ConfigObject LogObject LayoutObject ParamObject)
-        )
-    {
-        $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
-    }
-
     $Self->{UserID} = $Param{UserID};
-
-    $Self->{EncodeObject}    = $Param{EncodeObject} || Kernel::System::Encode->new( %{$Self} );
-    $Self->{TimeObject}      = $Param{TimeObject}   || Kernel::System::Time->new( %{$Self} );
-
-    $Self->{DBObject} = $Self->{LayoutObject}->{DBObject};
-
-    if ( $Param{TicketObject} ) {
-        $Self->{TicketObject} = $Param{TicketObject};
-    }
-
-    $Self->{ChecklistObject} = Kernel::System::PerlServices::TicketChecklist->new( %{$Self} );
-    $Self->{StatusObject}    = Kernel::System::PerlServices::TicketChecklistStatus->new( %{$Self} );
 
     return $Self;
 }
@@ -56,30 +38,35 @@ sub new {
 sub Run {
     my ( $Self, %Param ) = @_;
 
+    my $ParamObject     = $Kernel::OM->Get('Kernel::System::Web::Request');
+    my $ChecklistObject = $Kernel::OM->Get('Kernel::System::PerlServices::TicketChecklist');
+    my $StatusObject    = $Kernel::OM->Get('Kernel::System::PerlServices::TicketChecklistStatus');
+    my $LayoutObject    = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    my $ConfigObject    = $Kernel::OM->Get('Kernel::Config');
+
     # get template name
     #my $Templatename = $Param{TemplateFile} || '';
-    my $Templatename = $Self->{ParamObject}->GetParam( Param => 'Action' );
+    my $Templatename = $ParamObject->GetParam( Param => 'Action' );
 
     return 1 if !$Templatename;
     return 1 if !$Param{Templates}->{$Templatename};
-    return 1 if !$Self->{TicketObject};
 
     # define if rich text should be used
-    my ($TicketID) = $Self->{ParamObject}->GetParam( Param => 'TicketID' );
+    my ($TicketID) = $ParamObject->GetParam( Param => 'TicketID' );
 
     return 1 if !$TicketID;
 
-    my @ChecklistItems = $Self->{ChecklistObject}->TicketChecklistTicketGet(
+    my @ChecklistItems = $ChecklistObject->TicketChecklistTicketGet(
         TicketID => $TicketID,
     );
 
     return 1 if !@ChecklistItems;
 
-    my %StatusList   = $Self->{StatusObject}->TicketChecklistStatusList();
+    my %StatusList   = $StatusObject->TicketChecklistStatusList();
     my %StatusColors;
 
     for my $StatusID ( keys %StatusList ) {
-        my %StatusInfo = $Self->{StatusObject}->TicketChecklistStatusGet(
+        my %StatusInfo = $StatusObject->TicketChecklistStatusGet(
             ID => $StatusID,
         );
 
@@ -87,7 +74,7 @@ sub Run {
     }
 
     for my $Item ( @ChecklistItems ) {
-        $Item->{StatusSelect} = $Self->{LayoutObject}->BuildSelection(
+        $Item->{StatusSelect} = $LayoutObject->BuildSelection(
             Name        => 'ItemStatusWidget_' . $Item->{Position},
             Data        => \%StatusList,
             SelectedID  => $Item->{StatusID},
@@ -96,20 +83,20 @@ sub Run {
 
         $Item->{Color} = $StatusColors{ $Item->{StatusID} };
 
-        $Self->{LayoutObject}->Block(
+        $LayoutObject->Block(
             Name => 'Item',
             Data => $Item,
         );
     }
 
-    my $Snippet = $Self->{LayoutObject}->Output(
+    my $Snippet = $LayoutObject->Output(
         TemplateFile => 'TicketChecklistWidget',
         Data         => {
             TicketID => $TicketID,
         },
     );
 
-    my $Position = $Self->{ConfigObject}->Get( 'TicketChecklist::Position' ) || 'top';
+    my $Position = $ConfigObject->Get( 'TicketChecklist::Position' ) || 'top';
 
     if ( $Position eq 'bottom' ) {
         ${ $Param{Data} } =~ s{(</div> \s+ <div \s+ class="ContentColumn)}{ $Snippet $1 }xms;
@@ -118,8 +105,8 @@ sub Run {
         ${ $Param{Data} } =~ s{(<div \s+ class="SidebarColumn">)}{$1 $Snippet}xsm;
     }
 
-    my $JS = $Self->{LayoutObject}->Output(
-        Template => '<script type="text/javascript" src="$Config{"Frontend::JavaScriptPath"}PS.TicketChecklistWidget.js"></script>',
+    my $JS = $LayoutObject->Output(
+        Template => '<script type="text/javascript" src="[% Config("Frontend::JavaScriptPath") | html %]PS.TicketChecklistWidget.js"></script>',
     );
 
     ${ $Param{Data} } =~ s{</body}{$JS</body};
