f55e7b4665f42c561e9ec0c3d94aae5caa639a26
reneeb <info@perl-services.de>
2017-08-07 12:54:00 +0200

    make ticket deletion possible
        Without the custom ticket module, ticket deletion is impossible for tickets that have checklists. This is due to a
        foreign key constraint. The custom ticket module deletes the checklist items for the ticket first and the calls the
        original TicketDelete method

---
+++ Custom/Kernel/System/Ticket/Custom/TicketChecklist.pm
@@ -0,0 +1,47 @@
+# --
+# Copyright (C) 2017 Perl-Services.de, http://perl-services.de
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::Ticket::Custom::TicketChecklist;
+
+use strict;
+use warnings;
+
+our $ObjectManagerDisabled = 1;
+
+# disable redefine warnings in this scope
+{
+    no warnings 'redefine';
+
+    my $OrigCode = *Kernel::System::Ticket::TicketDelete{CODE};
+    
+    *Kernel::System::Ticket::TicketDelete = sub {
+        my ( $Self, %Param ) = @_;
+
+        my $ChecklistObject = $Kernel::OM->Get('Kernel::System::PerlServices::TicketChecklist');
+
+        # check needed stuff
+        if ( $Param{UserID} && $Param{TicketID} ) {
+            $ChecklistObject->TicketChecklistDelete(
+                TicketID => $Param{TicketID},
+            );
+        }
+    
+        return $Self->$OrigCode( %Param );
+    };
+
+}
+
+1;
+
+=head1 TERMS AND CONDITIONS
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
